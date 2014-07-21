47e56dc15ca5671e9a1bb622da1e88cafa249230
YinLonghai <1195219040@qq.com>
2014-07-21 19:32:29 +0800

    fix a bug

--- Sicilia/src/com/guotion/sicilia/im/util/OffineMessageManager.java
+++ Sicilia/src/com/guotion/sicilia/im/util/OffineMessageManager.java
@@ -53,9 +53,10 @@ public class OffineMessageManager {
 					String lastReadChatItemId = jsonObject.getString(chatGroup.get_id());
 					List<ChatItem> oneGroupItem = new LinkedList<ChatItem>();
 					List<ChatItem> items = chatItemManager.getMsgAfterOneMsg(chatHistory.get_id(),lastReadChatItemId);
+					oneGroupItem.addAll(items);
 					while(items.size() == 20) {
+						items = chatItemManager.getMsgAfterOneMsg(chatHistory.get_id(),oneGroupItem.get(oneGroupItem.size() - 1)._id);
 						oneGroupItem.addAll(items);
-						items = chatItemManager.getMsgAfterOneMsg(chatHistory.get_id(),lastReadChatItemId);
 					}
 					if (oneGroupItem.size() > 0) {
 						chatItems.put(chatGroup.get_id(),oneGroupItem);
@@ -74,48 +75,7 @@ public class OffineMessageManager {
 		}
 		return chatItems;
 	}
-	
-	/**
-	 * 获取有离线记录的，每个组的离线消息条数
-	 * @param userId
-	 * @return
-	 * @throws Exception
-	 */
-	public Map<String,Integer> getUnReadMessageCount(String userId) throws Exception{
-		Map<String,Integer> unReadMessageCount = new HashMap<String,Integer>();
-		LastRead lastRead = chatItemManager.getLastRead(userId);
-		JSONObject jsonObject = null;
-		try {
-			jsonObject = new JSONObject(lastRead.getDict());
-		} catch (Exception e) {
-			e.printStackTrace();
-			return unReadMessageCount; // 没有dict记录.
-		}
-		List<Object> groups = lastRead.getGroups(); // 有dict记录的组
-		Map<String, String> lastReadMap = new HashMap<String, String>();
-		for (Object object : groups) {
-			ChatGroup chatGroup = gson.fromJson(object + "", ChatGroup.class);
-			havenGet.put(chatGroup.get_id(), chatGroup.get_id());// 因为只是用于判断是否已经获取过了离线消息,所以保存的值是什么无需关心
-			ChatHistory chatHistory = chatItemManager.getMsgRelateHistory(chatGroup.get_id());
-			String lastReadChatItemId = jsonObject.getString(chatGroup.get_id());
-			List<ChatItem> oneGroupItem = chatItemManager.getMsgAfterOneMsg(chatHistory.get_id(),lastReadChatItemId);
-			int oneGroupMessageCount = oneGroupItem.size();
-			while(oneGroupItem.size() == 20){
-				//if count==20,the unread message may have more.
-				String lastReadItemId = oneGroupItem.get(oneGroupItem.size() - 1).get_id();
-				oneGroupItem = chatItemManager.getMsgAfterOneMsg(chatHistory.get_id(),lastReadItemId);
-				oneGroupMessageCount += oneGroupItem.size();
-			}
-			if(oneGroupMessageCount>0){
-				lastReadMap.put(chatGroup.get_id(),oneGroupItem.get(oneGroupItem.size() - 1).get_id());
-			}
-			unReadMessageCount.put(chatGroup.get_id(),oneGroupMessageCount);
-		}
-		if (lastReadMap.size() > 0) {
-			sendLastRead(lastReadMap, userId); // 同步服务器
-		}
-		return unReadMessageCount;
-	}
+
 
 	/**
 	 * 同步最后阅读消息到服务器
